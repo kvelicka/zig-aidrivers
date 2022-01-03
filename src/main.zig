a0fde02be2d1655095ccc9cbabfe0159855c01f0
Karl Velicka <karolis.velicka@gmail.com>
2022-01-03 15:50:18 +0000

    map drawing getting there

--- src/main.zig
+++ src/main.zig
@@ -17,14 +17,41 @@ const zimg = @import("zigimg");
 
 pub fn main() anyerror!void {
     const scale = 12;
-    var vehicles: [1]Vehicle = .{Vehicle{.x = 25, .y = 25, .a = 3.0, .colour = @enumToInt(Col.red)}};
+    const nvehicle = 2;
+    var vehicles: [nvehicle]Vehicle = .{
+        Vehicle{.x = 10, .y = 10, .angle = 3.0, .colour = @enumToInt(Col.red)},
+        Vehicle{.x = 10, .y = 10, .angle = 3.0, .colour = @enumToInt(Col.red)}
+    };
+    var configs: [nvehicle]Config = .{
+        Config{.c0 = 0.1, .c1 = 0.1},
+        Config{.c0 = 0.5, .c1 = 0.5},
+    };
+    const cfg = Sysconf{};
+
+    var prng = std.rand.DefaultPrng.init(blk: {
+        var seed: u64 = undefined;
+        try std.os.getrandom(std.mem.asBytes(&seed));
+        break :blk seed;
+    });
 
     var allocator = std.testing.allocator;
     // ppm_read map
     const p: Ppm = try readMapStdin(allocator);
     // ppm_to_map
     var map: Map = try Map.ppmToMap(&p, allocator);
-    std.log.debug("map={}", .{map.width});
+
+    // Format a map or ppm in shell
+    // // for (p.data.items) |col, ix| {
+    // var i: u32 = 0;
+    // while (i < p.width * p.height) : (i += 1) {
+    //     if (@rem(i, p.width) == 0) {
+    //         try std.fmt.format(std.io.getStdOut().writer(), "\n", .{});
+    //     }
+    //     // try std.fmt.format(std.io.getStdOut().writer(), "{d:03} ", .{col});
+    //     // _ = col;
+    //     try std.fmt.format(std.io.getStdOut().writer(), "{} ", .{map.get(@divFloor(i, p.width), @rem(i, p.height))});
+    // }
+
     // create overlay
     var overlay: Ppm = try Ppm.new(p.width * scale, p.height * scale, allocator);
     // draw map
@@ -32,29 +59,44 @@ pub fn main() anyerror!void {
     // ppm_create
     var out = try Ppm.new(p.width * scale, p.height * scale, allocator);
     // randomise configs
+    for (configs) |*config| {
+        randomise(config, &prng);
+    }
     // init  cars
-    // while true
+    for (vehicles) |*vehicle| {
+        vehicle.x = @intToFloat(f32, map.start_x);
+        vehicle.y = @intToFloat(f32, map.start_y);
+        vehicle.angle = map.start_angle;
+    }
     var t: u64 = 0;
     while (true) : (t += 1) {
         //   copy over overlay
         out.copy_from(&overlay);
+        // vehicles[0].angle += 0.1;
+        //   draw_vehicles
         drawVehicles(&out, &map, &vehicles);
-        vehicles[0].a += 0.1;
+        //   ppm_write out
         var writer = std.io.getStdOut().writer();
         out.write(writer);
-        //   draw_vehicles
-        //   ppm_write out
         //   for car in cars:
+        for (vehicles) |*vehicle, ix| {
         //     drive
-        //   eras dead cars
+            _ = drive(vehicle, &configs[ix], &map, &cfg);
+        }
+        //   erase dead cars
     }
+}
+
+fn randomise(config: *Config, prng: *std.rand.Xoshiro256) void {
+    var random = prng.random();
+    const exp: f32 = -32.0;
+    config.c0 = 1.0 * random.floatNorm(f32) * @exp2(exp);
+    config.c1 = 0.1 * random.floatNorm(f32) * @exp2(exp);
 }
 
 fn readMapStdin(allocator: std.mem.Allocator) !Ppm {
     var reader = std.io.getStdIn().reader();
     const ppm = try Ppm.from_reader(reader, allocator);
-    // _ = map.get_pixel(24, 4);
-    // _ = map.get_pixel(25, 4);
     return ppm;
 }
 fn getPixel(pixels: []zimg.color.Rgb24, info: zimg.image.ImageInfo, x: u32, y: u32) zimg.color.Color {
@@ -64,11 +106,84 @@ fn getPixel(pixels: []zimg.color.Rgb24, info: zimg.image.ImageInfo, x: u32, y: u
     return out.toColor();
 }
 
+fn drive(vehicle: *Vehicle, config: *Config, map: *Map, sysconf: *const Sysconf) bool {
+    _ = sysconf;
+    _ = config;
+    if (!alive(vehicle, map)) {
+        return false;
+    }
+
+    var senses = [_]f32{0.0} ** 3;
+    var angles = [_]f32 {PI/-4.0, 0, PI/4.0};
+    for (senses) |_, ix| {
+        senses[ix] = sense(vehicle.x, vehicle.y, vehicle.angle + angles[ix], map, null);
+    }
+
+    var steering: f32 = senses[2] * config.c0 - senses[0] * config.c0;
+    var throttle: f32 = senses[1] * config.c1;
+    if (throttle < sysconf.speedmin) {
+        throttle = sysconf.speedmin;
+    }
+    if (throttle > sysconf.speedmax) {
+        throttle = sysconf.speedmax;
+    }
+    vehicle.angle += if (@fabs(steering) > sysconf.control) std.math.copysign(f32, sysconf.control, steering) else steering;
+    vehicle.x += throttle * @cos(vehicle.angle);
+    vehicle.y += throttle * @sin(vehicle.angle);
+    return true;
+}
+
+fn sense(x: f32, y: f32, a: f32, map: *Map, maybe_ppm: ?*Ppm) f32 {
+    var dx: f32 = @cos(a);
+    var dy = @sin(a);
+    var d: i32 = 0;
+    while (true) : (d += 1) {
+        var bx: f32 = x + dx * @intToFloat(f32, d);
+        var by = y * dy * @intToFloat(f32, d);
+        var ix: i32 = @floatToInt(i32, bx);
+        var iy: i32 = @floatToInt(i32, by);
+        if (ix < 0 or (ix >= map.width) or (iy < 0) or (iy >= map.height)) {
+            break;
+        }
+        if (map.get(@intCast(u32, ix), @intCast(u32, iy)) > 0) {
+            break;
+        }
+        if(maybe_ppm) |ppm| {
+            var scale: u32 = ppm.width / map.width;
+            var py: u32 = 0;
+            while (py < scale) : (py += 1) {
+                var px: u32 = 0;
+                while (px < scale) : (px += 1) {
+                    ppm.set_pixel(@intCast(u32, ix) * scale + px, @intCast(u3, iy) * scale + py, @enumToInt(Col.red));
+                }
+            }
+        }
+    }
+    const fd = @intToFloat(f32, d);
+    return @sqrt(fd * dx * fd * dx + fd * dy * fd * dy);
+}
+
+fn alive(vehicle: *const Vehicle, map: *const Map) bool {
+    return map.get(@floatToInt(u32, vehicle.x), @floatToInt(u32, vehicle.y)) > 0;
+}
+
 const Vehicle = struct {
     x: f32,
     y: f32,
-    a: f32,
+    angle: f32,
     colour: u64,
+};
+
+const Config = struct {
+    c0: f32,
+    c1: f32,
+};
+
+const Sysconf = struct {
+    speedmin: f32 = 0.1,
+    speedmax: f32 = 0.5,
+    // Maximum turn per step
+    control: f32 = PI/128.0,
 };
 
 const Map = struct {
@@ -76,21 +191,20 @@ const Map = struct {
     height: u32,
     start_x: u32,
     start_y: u32,
-    sa: f32,
+    start_angle: f32,
     data: []u64,
 
     fn ppmToMap(ppm: *const Ppm, allocator: std.mem.Allocator) !@This() {
-        const item_size = 8 * @sizeOf(@TypeOf(ppm.data.items[0]));
-        const data_sz = (ppm.width * ppm.height + item_size - 1) / item_size * @sizeOf(u8);
+        const item_size = 8 * @sizeOf(u64);
+        const data_sz = (ppm.width * ppm.height + item_size - 1) / item_size * @sizeOf(u64);
         var map_buf = try allocator.alloc(u64, data_sz);
         std.log.debug("data_sz={}", .{data_sz});
-        // const item_size = 8@sizeOf(u64);
         var ret = Map{
             .width = ppm.width,
             .height = ppm.height,
             .start_x = ppm.width / 2,
             .start_y = ppm.height / 2,
-            .sa = 0,
+            .start_angle = 0,
             .data = map_buf,
         };
         var x: u32 = 0;
@@ -100,7 +214,7 @@ const Map = struct {
             while (x < ppm.width) : ( x += 1 ) {
                 const col = ppm.get_pixel(x, y);
                 if (Col.colour(col) == Col.green) {
-                    std.log.debug("new start! x,y={},{}", .{x, y});
+                    std.log.debug("new start loc! x,y={},{}", .{x, y});
                     ret.start_x = x;
                     ret.start_y = y;
                 }
@@ -114,7 +228,8 @@ const Map = struct {
                 if (Col.colour(col) == Col.blue) {
                     const fy: f32 = @intToFloat(f32, (y - ret.start_y));
                     const fx: f32 = @intToFloat(f32, x - ret.start_x);
-                    ret.sa = std.math.atan2(f32, fy, fx);
+                    ret.start_angle = std.math.atan2(f32, fy, fx);
+                    std.log.debug("new start angle! a={}", .{ret.start_angle});
                 }
             }
         }
@@ -125,7 +240,6 @@ const Map = struct {
                 const col = ppm.get_pixel(x, y);
                 const v = @as(u64, @boolToInt(col >> 16 > 0x7f));
                 const pixel: usize = y * ppm.width + x;
-                // ret.data[pixel/item_size] |= v << (pixel % item_size);
                 ret.data[pixel/item_size] |= v << @intCast(u6, @mod(pixel, item_size));
             }
         }
@@ -133,7 +247,7 @@ const Map = struct {
 
     }
 
-    fn get(self: *@This(), x: u32, y: u32) u32 {
+    fn get(self: *const @This(), x: u32, y: u32) u32 {
         const pixel = y * self.width + x;
         const item_size = 8 * @sizeOf(@TypeOf(self.data[0]));
         return @intCast(u32, self.data[pixel / item_size] >> @intCast(u6, @mod(pixel, item_size)) & 1);
@@ -198,8 +312,7 @@ const Ppm = struct {
         std.log.info("ppm_ver={s} w={} h={} max_col_value={s}", .{ ppm_ver, width, height, max_col_value });
 
         var out = try new(width, height, allocator);
-        const data_sz = @as(usize, width * height * 3);
-        try reader.readAllArrayList(&out.data, data_sz);
+        try reader.readNoEof(out.data.items);
         return out;
     }
 
@@ -239,8 +352,6 @@ const Ppm = struct {
         const header = std.fmt.bufPrint(&buf, "P6\n{d} {d}\n255\n", .{self.width, self.height}) catch unreachable;
         _ = writer.write(header) catch unreachable;
         _ = writer.write(self.data.items[0..self.data.items.len]) catch unreachable;
-        // _ = writer.write("foo\n") catch unreachable;
-        // std.log.debug("hdr {d} data {d}", .{hdr_size, data_size});
     }
 };
 
@@ -266,36 +377,11 @@ fn drawVehicles(ppm: *Ppm, map: *Map, vehicles: []Vehicle) void {
         while (d < scale * 2) : (d += 1) {
             var j: i32 = -@divTrunc(scale, 2);
             while (j < @divTrunc(scale, 2)) : (j += 1) {
-                // const x: f32 = @intToFloat(f32, scale) * v.x;
-                const x: f32 = @intToFloat(f32, scale) * v.x + @intToFloat(f32, j) * @cos(v.a - PI / 2.0) + @intToFloat(f32, d) * @cos(v.a) / 2.0;
-                const y: f32 = @intToFloat(f32, scale) * v.y + @intToFloat(f32, j) * @sin(v.a - PI / 2.0) + @intToFloat(f32, d) * @sin(v.a) / 2.0;
-                std.log.debug("{} {}", .{x, y});
+                const x: f32 = @intToFloat(f32, scale) * v.x + @intToFloat(f32, j) * @cos(v.angle - PI / 2.0) + @intToFloat(f32, d) * @cos(v.angle) / 2.0;
+                const y: f32 = @intToFloat(f32, scale) * v.y + @intToFloat(f32, j) * @sin(v.angle - PI / 2.0) + @intToFloat(f32, d) * @sin(v.angle) / 2.0;
                 ppm.set_pixel(@floatToInt(u32, x), @floatToInt(u32, y), v.colour);
             }
         }
 
     }
 }
-
-// fn zigimg() !void {
-//     var allocator = std.testing.allocator;
-//     var ss = std.io.StreamSource{ .file = std.io.getStdIn() };
-//     var storage: ?zimg.color.ColorStorage = null;
-//     const info = try PPM.readForImage(allocator, ss.reader(), undefined, &storage);
-//     std.log.debug("image_info={}", .{info});
-
-//         var it = zimg.color.ColorStorageIterator.init(&storage.?);
-//         while (it.next()) |itm| {
-//             _ = itm;
-//             break;
-//             // std.log.debug("itm={}", .{itm});
-//         }
-//         switch(storage.?) {
-//             .Rgb24 => |data| _ = {
-//                 _ = getPixel(data, info, 24, 4);
-//                 _ = getPixel(data, info, 25, 4);
-//                 // _ = getPixel(data, info, 100, 40);
-//             },
-//             else => unreachable,
-//     }
-// }
